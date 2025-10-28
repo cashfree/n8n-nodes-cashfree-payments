79437e9b9feec3abd9b446a68f52b450c65b6067
Aditya Chauhan <aditya.chauhan@cashfree.com>
2025-10-28 17:49:47 +0530

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
-const { src, dest } = require('gulp');
+const { src, dest, parallel } = require('gulp');
 
 function buildIcons() {
 	return src('nodes/CashfreePayments/*.{png,svg}')
 		.pipe(dest('dist/nodes/CashfreePayments/'));
 }
 
+function buildNodes() {
+	return src('nodes/**/*.js')
+		.pipe(dest('dist/nodes/'));
+}
+
+function buildCredentials() {
+	return src('credentials/**/*.js')
+		.pipe(dest('dist/credentials/'));
+}
+
 exports['build:icons'] = buildIcons;
+exports['build:nodes'] = buildNodes;
+exports['build:credentials'] = buildCredentials;
+exports['build'] = parallel(buildIcons, buildNodes, buildCredentials);
